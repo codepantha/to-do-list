5f9938e5182f42e8233250e64cd35c14efc0926e
AlexRS90 <aramosbengy@gmail.com>
2021-11-04 09:11:53 -0600

    Test for add task functionality

--- src/__tests__/task.test.js
+++ src/__tests__/task.test.js
@@ -6,6 +6,21 @@ import { deleteTodo } from '../task';
 import { getTodoList, saveTodoList } from '../functions';
 
 describe('add and delete tasks', () => {
+  test('adds a single to-do item to the list', () => {
+    const todo = [];
+    saveTodoList(todo); // save the empty array of to-do list items
+    const numberOfItemsBeforeSave = getTodoList().length;
+    // save an actual to-do item
+    todo.push({
+      index: 1,
+      description: 'task 1',
+      completed: false,
+    });
+    saveTodoList(todo);
+    const numberOfItemsAfterSave = getTodoList().length;
+    expect(numberOfItemsAfterSave).toBe(numberOfItemsBeforeSave + 1);
+  });
+  
   test('deletes a single to-do item from the list', () => {
     const todo = [
       {
