f7630b37653c705b911f129b9c57d3ead93ec9fc
AlexRS90 <aramosbengy@gmail.com>
2021-11-03 21:57:35 -0600

    Setup jest and babel

---
+++ src/__mocks__/task.js
@@ -0,0 +1,6 @@
+
+console.log("mooooocks");
+export const deleteTodo = (id) => {
+  const local = id;
+  return local;
+};
--- src/task.test.js
+++ src/task.test.js
@@ -1,27 +1,32 @@
 import { test } from '@jest/globals';
 import { deleteTodo } from './task.js';
+jest.mock('./task');
 
 test('Testing', () => {
-  console.log("hello");
-  const storage = [
-    {
-      id: 1,
-      description: 'New task', 
-      completed: false,
-    },
-    {
-        id: 2,
-        description: 'New task2', 
-        completed: false,
-    },
-    {
-        id: 3,
-        description: 'New task3', 
-        completed: false,
-    },
-  ];
-  const numberOfItems = storage.length;
-  deleteTodo(1);
-  console.log(storage.length)
-  expect(storage.length).toBe(numberOfItems - 1);
+  
+expect(deleteTodo({ name: 'thing', index: 3 }, [{ name: 'thing', index: 3 }, { name: 'other thing', index: 2 }]))
+  .toEqual(expect.objectContaining({ length: 1, local: [{ name: 'other thing', index: 0 }] }));
+  
+  // console.log("hello");
+  // const storage = [
+  //   {
+  //     id: 1,
+  //     description: 'New task', 
+  //     completed: false,
+  //   },
+  //   {
+  //       id: 2,
+  //       description: 'New task2', 
+  //       completed: false,
+  //   },
+  //   {
+  //       id: 3,
+  //       description: 'New task3', 
+  //       completed: false,
+  //   },
+  // ];
+  // const numberOfItems = storage.length;
+  // deleteTodo(1);
+  // console.log(storage.length)
+  // expect(storage.length).toBe(numberOfItems - 1);
 });
