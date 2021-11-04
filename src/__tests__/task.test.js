024e96ccb70a786d1c91d405e3617bb21c8d47f9
AlexRS90 <aramosbengy@gmail.com>
2021-11-04 10:35:01 -0600

    Add mocks for localstorage

--- src/__tests__/task.test.js
+++ src/__tests__/task.test.js
@@ -4,10 +4,11 @@
 
 import { deleteTodo } from '../task';
 import { getTodoList, saveTodoList } from '../functions';
+jest.mock('../functions');
 
 describe('add and delete tasks', () => {
+  const todo = [];
   test('adds a single to-do item to the list', () => {
-    const todo = [];
     saveTodoList(todo); // save the empty array of to-do list items
     const numberOfItemsBeforeSave = getTodoList().length;
     // save an actual to-do item
@@ -22,24 +23,9 @@ describe('add and delete tasks', () => {
   });
 
   test('deletes a single to-do item from the list', () => {
-    const todo = [
-      {
-        index: 1,
-        description: 'task 1',
-        completed: false,
-      },
-      {
-        index: 2,
-        description: 'task 2',
-        completed: false,
-      },
-    ];
-
-    saveTodoList(todo);
     const numberOfItemsBeforeDelete = getTodoList().length;
     deleteTodo(1);
     const numberOfItemsAfterDelete = getTodoList().length;
-
     expect(numberOfItemsAfterDelete).toBe(numberOfItemsBeforeDelete - 1);
   });
 });
